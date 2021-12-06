c498b833d9933f830b985a397caef85eb807cd5a
Dominisam98 <dominiqua6@outlook.com>
2021-12-06 14:27:59 -0500

    sign up and login button css

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -69,7 +69,7 @@ function SignupFormPage() {
           required
         />
       </label>
-      <button type="submit">Sign Up</button>
+      <button className='submit' type="submit">Sign Up</button>
     </form>
   );
 }
