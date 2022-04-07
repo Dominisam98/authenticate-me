22e1bb1da81743ef7ab6df891ccdf0d6cbcd42e3
Dominisam98 <dominiqua6@outlook.com>
2022-04-07 14:08:15 -0400

    update index

--- frontend/src/components/MainPage/index.js
+++ frontend/src/components/MainPage/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
-import AllEntries from '../AllEntries/allEntries';
+
 import './Main.css';
 
 
